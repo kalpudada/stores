b4068eb503b4f0ca30b2f174705ecb0244c24b42
Kalpnath Rao <kalpnath@engg.voylla.com>
2016-11-01 22:43:43 +0530

    New routes added for store app

--- stores/config/routes.rb
+++ stores/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'welcome/index'
 
-  root 'welcome#index'
+  resources :stores
+  root 'stores#index'
+  get 'stores/:id' => 'stores#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
